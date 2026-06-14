d7b4974c86f1d8d521651024df50074e8075df37
Kai Lochbaum <lochbaum@salient-doremus.de>
2026-06-14 16:40:07 +0200

    WOD: Following Custom Language Support Tutorial (Step 7 Annotator WIP - Element Names)

--- src/main/java/org/wocommunity/plugins/intellij/wod/WODAnnotator.java
+++ src/main/java/org/wocommunity/plugins/intellij/wod/WODAnnotator.java
@@ -5,18 +5,30 @@ import com.intellij.ide.highlighter.JavaHighlightingColors;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.editor.colors.CodeInsightColors;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.colors.impl.TextAttributeKeyColor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.PsiShortNamesCache;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlTag;
+import kotlinx.html.HTMLTag;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.wocommunity.plugins.intellij.wod.psi.WODBinding;
-import org.wocommunity.plugins.intellij.wod.psi.WODComponent;
-import org.wocommunity.plugins.intellij.wod.psi.WODElement;
-import org.wocommunity.plugins.intellij.wod.psi.WODKeyPath;
+import org.jetbrains.annotations.Nullable;
+import org.wocommunity.plugins.intellij.wod.psi.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 /*
  * An Annotator helps highlight and annotate any code based on specific rules.
@@ -29,35 +41,77 @@ final class WODAnnotator implements Annotator {
 
     @Override
     public void annotate(@NotNull PsiElement psiElement, @NotNull AnnotationHolder holder) {
-        if (psiElement instanceof WODElement element) {
-            annotateElement(element, holder);
-        }
-        else if (psiElement instanceof WODComponent component) {
-            annotateComponent(component, holder);
-        }
-        else if (psiElement instanceof WODBinding binding) {
-            annotateBinding(binding, holder);
-        }
-        else if (psiElement instanceof WODKeyPath keyPath) {
-            annotateKeyPath(keyPath, holder);
+        switch (psiElement) {
+            case WODElement element -> annotateElement(element, holder);
+            case WODComponent component -> annotateComponent(component, holder);
+            case WODBinding binding -> annotateBinding(binding, holder);
+            case WODKeyPath keyPath -> annotateKeyPath(keyPath, holder);
+            default -> {}
         }
     }
 
     private void annotateElement(@NotNull WODElement element, @NotNull AnnotationHolder holder) {
-        // TODO: find out which elements are named in the HTML file and match against the identifier of this element
-        //       -> Identifier is used => normal color
-        //       -> Identifier is not used => unused color
-        // TODO: Check duplicate identifiers => error
-        // Im HTML im Eclipse Plugin steht: The element 'LabelTargetUrl' is not defined in AdminModalEditExternalReference.wod
-        // There is no element named 'LocClose' in your component HTML file
-        holder.newAnnotation(HighlightSeverity.ERROR, "Unresolved identifier")
+        // Find out which elements are named in the HTML file and match against the identifier of this element
+        //       -> Identifier is not used => weak warning
+        List<String> namesFromHtml = getNamesFromHTMLTemplate(element.getContainingFile());
+        if (namesFromHtml.stream().noneMatch(element.getIdentifier().getText()::equals)) {
+            holder.newAnnotation(HighlightSeverity.WEAK_WARNING, "There is no element named '" + element.getIdentifier().getText() + "' in your component HTML file")
+                    .range(element)
+                    .create();
+        }
+
+        // Check duplicate identifiers => error
+        PsiElement previousDeclaration = element.getParent().getPrevSibling();
+        while (previousDeclaration != null) {
+            if (previousDeclaration instanceof WODDeclaration && ((WODDeclaration) previousDeclaration).getWODElement().getIdentifier().getText().equals(element.getIdentifier().getText())) {
+                holder.newAnnotation(HighlightSeverity.ERROR, "Element '" + element.getIdentifier().getText() + "' is already defined above")
+                        .range(element)
+                        .create();
+                break;
+            }
+            previousDeclaration = previousDeclaration.getPrevSibling();
+        }
+
+        // Nothing to complain about => normal color
+        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
                 .range(element)
-                .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
+                .textAttributes(TextAttributesKey.find("HTML_ATTRIBUTE_NAME"))
                 .create();
     }
 
+    // FIXME: This should probably be somewhere central or close to the HTML-File? :o
+    private List<String> getNamesFromHTMLTemplate(PsiFile wodFile) {
+        List<String> elementNames = new ArrayList<>();
+
+        String componentName = wodFile.getName().replace(".wod", "");
+        if (wodFile.getParent() == null) {
+            return elementNames;
+        }
+
+        PsiFile htmlFile = wodFile.getParent().findFile(componentName + ".html");
+        if (htmlFile == null) {
+            return elementNames;
+        }
+
+        Collection<XmlTag> tags = PsiTreeUtil.findChildrenOfType(htmlFile, XmlTag.class);
+        if (tags.isEmpty()) {
+            return elementNames;
+        }
+
+        for (XmlTag tag : tags) {
+            if (tag.getName().equals("webobject")) {
+                @Nullable XmlAttribute name = tag.getAttribute("name");
+                if (name != null) {
+                    elementNames.add(name.getValue());
+                }
+            }
+        }
+
+        return elementNames;
+    }
+
     private void annotateComponent(@NotNull WODComponent component, @NotNull AnnotationHolder holder) {
-        // TODO: Validate component name against WebObjects tag names or java classes...
+        // Validate component name against WebObjects java classes
         //       -> Valid => normal color
         //       -> Invalid => error annotation
 
@@ -82,10 +136,10 @@ final class WODAnnotator implements Annotator {
         PsiClass[] candidateClasses = cache.getClassesByName(className, scope);
         for (PsiClass candidateClass : candidateClasses) {
             if (candidateClass.isInheritor(baseClass, true)) {
-                // Success
+                // Nothing to complain about => normal color
                 holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
                         .range(component)
-                        .textAttributes(JavaHighlightingColors.CLASS_NAME_ATTRIBUTES)
+                        .textAttributes(TextAttributesKey.find("HTML_TAG_NAME"))
                         .create();
                 return;
             }
@@ -94,7 +148,6 @@ final class WODAnnotator implements Annotator {
         if (candidateClasses.length == 0) {
             holder.newAnnotation(HighlightSeverity.ERROR, "The class for '" + className + "' is missing")
                     .range(component)
-                    //.highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
                     .create();
             return;
         }
