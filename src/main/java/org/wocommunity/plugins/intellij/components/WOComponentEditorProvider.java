80045b46140828a91fa1a36795f94ee2e332d6bb
Kai Lochbaum <lochbaum@salient-doremus.de>
2026-06-15 20:26:56 +0200

    Improve File opening: Selecting a file inside the .wo folder also opens the split editor.

--- src/main/java/org/wocommunity/plugins/intellij/components/WOComponentEditorProvider.java
+++ src/main/java/org/wocommunity/plugins/intellij/components/WOComponentEditorProvider.java
@@ -6,10 +6,13 @@ import com.intellij.notification.Notifications;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorPolicy;
 import com.intellij.openapi.fileEditor.FileEditorProvider;
+import com.intellij.openapi.fileEditor.TextEditor;
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+import org.wocommunity.plugins.intellij.WOUtil;
 
 import java.io.IOException;
 
@@ -17,23 +20,26 @@ public class WOComponentEditorProvider implements FileEditorProvider, DumbAware 
 
     @Override
     public boolean accept(@NotNull Project project, @NotNull VirtualFile file) {
-        // Only accept .wo folders
-        return file.isDirectory() && file.getName().endsWith(".wo");
+        // Accept .wo folders or files inside a .wo folder
+        return WOUtil.fileIsComponent(file) || WOUtil.fileIsPartOfComponent(file);
     }
 
     @Override
     public @NotNull FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile file) {
         try {
-            return new WOComponentEditor(project, file);
+            if (WOUtil.fileIsComponent(file)) {
+                return new WOComponentEditor(project, file);
+            }
+            return new WOComponentEditor(project, file.getParent());
         } catch (IOException e) {
             Notifications.Bus.notify(new Notification(
                     "WOComponent", "File Not Found",
                     file.getName() + " cannot be openend: " + e.getMessage(),
                     NotificationType.WARNING
             ));
-
-            return null;
         }
+
+        return TextEditorProvider.getInstance().createEditor(project, file);
     }
 
     @Override
